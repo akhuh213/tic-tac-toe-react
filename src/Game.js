2a04a280d6b5be7dce0b7cefaa1094d86eedc2e6
Alice Aekyung Huh <aklee213@gmail.com>
2020-08-29 23:27:25 -0400

    [tic-tac-toe] Add react

---
+++ src/Game.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Board from './Board';
+
+function Game() {
+  return (
+    <div className="Game">
+      <Board />
+    </div>
+
+
+  );
+}
+
+export default Game;
